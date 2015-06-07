718fe81c49a3317f4affc86584af51236c4f71d5
Krishna Ramdeep <kramdeep3@gatech.edu>
2015-06-07 17:02:55 -0500

    scalajs: jquery

--- src/main/scala/simplegame/HelloWorld.scala
+++ src/main/scala/simplegame/HelloWorld.scala
@@ -3,16 +3,24 @@ package simplegame
 import scala.scalajs.js.JSApp
 import org.scalajs.dom
 import dom.document
+import org.scalajs.jquery.jQuery
+
+import scala.scalajs.js.annotation.JSExport
 
 object TutorialApp extends JSApp {
   def main(): Unit = {
-    appendPar(document.body, "Hello World")
+    jQuery("body").append("<p>[message]</p>")
     println("Hello world!")
   }
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
+//  def appendPar(targetNode: dom.Node, text: String): Unit = {
+//    val parNode = document.createElement("p")
+//    val textNode = document.createTextNode(text)
+//    parNode.appendChild(textNode)
+//    targetNode.appendChild(parNode)
+//  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    jQuery("body").append("<p>[message]</p>")
   }
 }
